1f33942969fa140b1c59a2db4042e5b8d468c1a6
5h140w <5h140w23@gmail.com>
2023-09-21 19:48:26 +0100

    pull request

--- controllers/usercontrollers.js
+++ controllers/usercontrollers.js
@@ -1,31 +1,31 @@
 const user = require("../model/user");
 
-module.exports.login = async (req,res)=>{
-    const { email,password } = req.body;
+module.exports.login = async (req, res) => {
+    const {email, password} = req.body;
 
-    const theuser = await user.findOne({email,password});
+    const theuser = await user.findOne({email, password});
 
     if (theuser !== null) {
         return res.status(200).json(theuser);
     } else {
-        return res.status(400).json({message:"user not found"});
-}
+        return res.status(400).json({message: "user not found"});
+    }
 };
 
-module.exports.register =(req,res) => {
-    const {username,email,password} =req.body;
+module.exports.register = (req, res) => {
+    const {username, email, password} = req.body;
 
-    const newuser = new user ({
+    const newuser = new user({
         username,
         email,
         password,
     });
-console.log (newuser)
+    console.log(newuser)
     newuser.save().then(
-        (res)=>{
-            return res.status(200).json({message:"wlecome  new user"});   
+        () => {
+            return res.status(200).json({message: "wlecome  new user"});
         }).catch(
-            (err) =>{
-                return res.status(400).json({message:err.message});
+            (err) => {
+                return res.status(400).json({message: err.message});
             });
 };
