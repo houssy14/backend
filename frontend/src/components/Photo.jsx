fe71b989c4108cae57e7f3cdc56908a47a9159c6
hossy14 <touhamiihossy@gmail.com>
2023-10-09 19:02:58 +0100

    houssem-1

--- frontend/src/components/Photo.jsx
+++ frontend/src/components/Photo.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-import image from './houssem.JPG'
+import GomyImage from './Gomy.png';
 export default function Photo() {
   return (
+    <div>
+  <br></br>
+  <br></br>
+    <img src={GomyImage} alt="Gomy" />
+    <br></br>
+    <br></br>
+    <iframe width="300" height="315" src="https://www.youtube.com/embed/rrRX9vo5dNo?si=byTMvlQjkYkej6gM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe width="300" height="315" src="https://www.youtube.com/embed/ZupLXWWRGAY?si=SoohXroQyNXbhGHK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
     
-    <div><img src={image} alt="" srcset="" /></div>
+    
   )
 }
